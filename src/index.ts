7da98e01ecec1de0b117350ff6aef9e2e55297ec
Tomatkungen <mr_a2@hotmail.com>
2024-01-06 02:21:28 +0100

    fix: package setup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
-import {git_log} from './public/git_log';
-import {git_reference} from './public/git_reference';
-import {git_status} from './public/git-status';
+import { git_log } from './public/git_log';
+import { git_reference } from './public/git_reference';
+import { git_status } from './public/git-status';
 import { lg } from './util/pr_lg';
 
-export {git_log, git_reference, git_status};
+export { git_log, git_reference, git_status };
 
-(async () => {
-
-    const status = await git_status();
-    lg('status', status);
-    
-    // const log = await git_log();
-    // lg('log', log);
-
-    // const refs = await git_reference();
-    // lg('refs', refs);
-
-})()
+(() => {
+    lg('nodejs-git-json');
+})();
